764ba86e78250bb25ec0fa8deea924bbcf9db822
Martin Ouellet <moa21@sfu.ca>
2016-09-14 20:54:47 -0700

    223

--- app/src/main/java/com/mgodevelopment/currencyapp/utils/NotificationUtils.java
+++ app/src/main/java/com/mgodevelopment/currencyapp/utils/NotificationUtils.java
@@ -1,10 +1,22 @@
 package com.mgodevelopment.currencyapp.utils;
 
 import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
 import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.mgodevelopment.currencyapp.Constants;
+import com.mgodevelopment.currencyapp.MainActivity;
+import com.mgodevelopment.currencyapp.R;
 
 import java.util.List;
 
@@ -21,6 +33,32 @@ public class NotificationUtils {
 
         if (isAppInBackground(context)) {
 
+            int icon = R.mipmap.ic_launcher;
+            Intent intent = new Intent(context, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+            NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+            Notification notification = builder
+                    .setSmallIcon(icon)
+                    .setTicker(title)
+                    .setWhen(0)
+                    .setAutoCancel(true)
+                    .setContentTitle(title)
+                    .setStyle(inboxStyle)
+                    .setContentIntent(resultPendingIntent)
+                    .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                    .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), icon))
+                    .setContentText(message)
+                    .build();
+
+            NotificationManager notificationManager = (NotificationManager)
+                    context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(Constants.NOTIFICATION_ID, notification);
+
+        } else {
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
         }
 
     }
